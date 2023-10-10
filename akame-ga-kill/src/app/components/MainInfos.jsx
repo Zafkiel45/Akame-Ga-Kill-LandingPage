6d0eed350fdfa9d55f19e073fe838b8611215b56
Murilo Silva <zafkielc@gmail.com>
2023-10-10 13:35:40 -0300

    Alterando e fatorando o conteúdo inicial
    Fazendo algumas alterações na logo "Akame ga Kill" e ajustando o posicionamento.  Fatorei o código tornando mais intuitivo e fácil de ler

--- akame-ga-kill/src/app/components/MainInfos.jsx
+++ akame-ga-kill/src/app/components/MainInfos.jsx
@@ -3,18 +3,18 @@ import Logo_Akame from '../../../public/images/logoK.webp'
 
 export const MainInfos = () => {
     return (
-        <div className="z-0 top-0 left-0 p-10 h-[797px] gap-9 py-14 items-start flex flex-col justify-start bg-no-repeat w-screen object-cover bg-cover bg-center bg-[url('../../public/images/banner.webp')]">
-            <figure className="relative right-24 w-3/6 h-3/6 object-cover">
-                <Image src={Logo_Akame} fill={true} />
+        <div className="z-0 p-10 font-semibold text-white  h-[797px] gap-9 py-14 items-start flex flex-col justify-start bg-no-repeat w-screen bg-cover bg-center bg-[url('../../public/images/banner.webp')]">
+            <figure className="relative right-[5rem] w-[700px] h-[408px]">
+                <Image className="object-cover" src={Logo_Akame} alt=""  fill={true} />
             </figure>
-            <div className="w-3/5 z-10 relative bottom-32"> 
-                <p className="text-white font-bold w-full text-4xl">
+            <div className="w-3/5 z-10 relative bottom-[10rem]"> 
+                <p className="w-full text-4xl">
                     Acompanhe a jornada de Tatsumi
                     e a Night Raider em busca de tornar
                     o império um Lugar melhor agora Mesmo!
                 </p>
             </div>
-            <button className="w-60 rounded-md relative bottom-32 hover:text-red-claro-text text-white font-medium transition-colors hover:bg-black bg-red-claro p-3">
+            <button className="w-60 rounded-md relative bottom-[9rem] transition-colors hover:bg-black bg-red-claro p-3">
                 Assista Agora
             </button>
         </div>
